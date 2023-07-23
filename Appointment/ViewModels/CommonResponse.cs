0d07b4f41b82deb67f99bdfb049fab57465318bd
IT-FEBRIAN <IT-FEBRIAN@IT-FEBRIAN>
2023-07-23 17:39:28 +0700

    Add Api controller

--- Appointment/ViewModels/CommonResponse.cs
+++ Appointment/ViewModels/CommonResponse.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace AppointmentScheduling.Models.ViewModels
+namespace Appointment.ViewModels
 {
     public class CommonResponse<T>
     {
